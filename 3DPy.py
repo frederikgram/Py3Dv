8970ef3f2d8427103cf89924dc506beb8c08bef5
frederikgram <frederikxyz@hotmail.com>
2018-10-09 04:20:46 +0200

    Initialized project

---
+++ 3DPy.py
@@ -0,0 +1,37 @@
+import pygame
+import sys
+import math
+
+# Initialize pygame
+
+pygame.init()
+w, h = 400, 400
+screen = pygame.display.set_mode((w, h))
+
+
+class obj:
+
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+
+def load_obj(path_to_obj):
+    """ Returns a list of vertices from a given .obj file """
+
+    with open(path_to_obj, 'r') as f:
+        lines = [line for line in f.readlines() if '#' not in line]
+        vertices = [line.split()[1:4] for line in lines if line[0] == 'v']
+
+    return vertices
+
+
+def find_edges(vertices):
+    """ Returns a list of edges in a given list of vertices """
+    edges = []
+    return edges
+
+
+if __name__ == "__main__":
+    """ 3DPy startup script """
+
+    obj = load_obj("./test.obj")
